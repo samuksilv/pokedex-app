d34cd010fec146964a7ccaf0f07ecaa09170a03e
Samuel Silva De Carvalho <samuel.carvalho@consinco.com.br>
2019-05-07 09:54:16 -0300

    correção no layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
 
 const routes: Routes = [
-  { path:"home", component:HomeComponent},
+  { path:"home", component:HomeComponent},  
+  { path:"pokemon/:id", component: PokemonDetailComponent},  
   {path:"**", redirectTo: "/home"}
 ];
 
